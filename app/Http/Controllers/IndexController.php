f98b11e7b78efc99de152acb5ffba02ee36e4889
evvanted <evvanted@gmail.com>
2019-06-25 14:35:44 +0300

    add models

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Page;
+use App\Portfolio;
+use App\Service;
+use App\People;
+
 class IndexController extends Controller
 {
     public function execute(Request $request){
+        $pages = Page::all();
+        $portfolios = Portfolio::get(array('name', 'filter', 'images'));
+        $services = Service::where('id', '<', 20)->get();
+        $peoples = People::take(3)->get();
         return view('layouts.site');
     }
 }
