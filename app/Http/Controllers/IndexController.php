4426cc945098b8cf926da58c11ba39a17a518986
evvanted <evvanted@gmail.com>
2019-06-26 14:08:51 +0300

    form and mail

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,11 +9,59 @@ use App\Portfolio;
 use App\Service;
 use App\People;
 
+use Illuminate\Support\Facades\Mail;
+
 use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
     public function execute(Request $request){
+
+        if (session('status')) {
+            $request->session()->forget('status'); // удаление из сессии
+        }
+
+        if($request->isMethod('post')){
+//            dd($request);
+            $messages = [
+                'required' => "Поле :attribute обязательно к заполнению",
+                'email' => "Поле :attribute должно соответствовать email"
+            ];
+            $this->validate($request, [
+                'name' => 'required|max:255',
+                'email' => 'required|email',
+                'text' => 'required',
+            ], $messages);
+
+            $data = $request->all();
+//            dd($data);
+
+//            $result = Mail::send('site.email', ['data' => $data], function($message) use($data){
+//                $mail_admin = env('MAIL_ADMIN');
+//
+//                $message->from($data['email'], $data['name']);
+//                $message->to($mail_admin)->subject('Question');
+//
+//            });
+
+            Mail::send('site.email', ['data'=>$data], function($message) use ($data) {
+                // Почта куда приходят письма
+                $mailAdmin = env('MAIL_ADMIN');
+//                dd($mailAdmin);
+                // Данные для отправки
+                $message->from('hello@app.com');
+                // Куда отправить и название темы
+                $message->to('hello@app.com')->subject('Question');
+                session(['status' => 'Email is send']); // запись в сессию
+                return redirect()->route('home');
+            });
+
+//            if($result){
+//                return redirect()->route('home')->with('status', 'Email is sent');
+//            }
+
+        }
+
         $pages = Page::all();
         $portfolios = Portfolio::get(array('name', 'filter', 'images'));
         $services = Service::where('id', '<', 20)->get();
