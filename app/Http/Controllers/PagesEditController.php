5b6eac8fdbe09963e4d9023d19ac18a066fed864
evvanted <evvanted@gmail.com>
2019-06-27 16:54:10 +0300

    pages edit

---
+++ app/Http/Controllers/PagesEditController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Page;
+use Illuminate\Support\Facades\Validator;
+
+class PagesEditController extends Controller
+{
+    //
+
+//    public function execute($id){
+    public function execute(Page $page, Request $request){
+//        $page =Page::find($id);
+
+        if($request->isMethod('post')){
+            $input = $request->except('_token');
+
+            $massages = [
+                'required'=>'Поле :attribute обязательно к заполнению',
+                'unique'=>'Поле :attribute должно быть уникальным'
+            ];
+
+            $validator = Validator::make($input, [
+                'name'=>'required|max:255',
+                'alias'=>'required|max:255|unique:pages,alias,'.$input['id'],
+                'text'=>'required'
+            ], $massages);
+
+            if($validator->fails()){
+                return redirect()->route('pagesEdit',['page'=>$input['id']])->withErrors($validator)->withInput();
+            }
+
+            if($request->hasFile('images')){
+                $file = $request->file('images');
+                $file->move(public_path().'/assets/img', $file->getClientOriginalName());
+                $input['images'] = $file->getClientOriginalName();
+            } else {
+                $input['images'] = $input['old_images'];
+            }
+            unset($input['old_images']);
+            $page->fill($input);
+            if($page->update()){
+                return redirect('admin')->with('status','Страница обновлена');
+            }
+
+
+        }
+
+        $old = $page->toArray();
+
+        if(view()->exists('admin.pages_edit')){
+            $data = [
+                'title' => 'Редактирование страницы - '.$old['name'],
+                'data' => $old
+            ];
+
+            return view('admin.pages_edit', $data);
+        }
+    }
+}
